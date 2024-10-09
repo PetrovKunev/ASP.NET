b6ddf4414b52a2e4f22a01138166ceafd2a75b7d
Yavor Petrov <147103195+PetrovKunev@users.noreply.github.com>
2024-10-09 16:49:14 +0300

    Views

---
+++ CSharpWeb_CinemaApp_Sept2024/CinemaApp.Web/Controllers/MovieController.cs
@@ -0,0 +1,64 @@
+﻿using CinemaApp.Data;
+using CinemaApp.Data.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlTypes;
+
+namespace CinemaApp.Web.Controllers
+{
+    public class MovieController : Controller
+    {
+        private readonly CinemaDbContext dbContext;
+
+        public MovieController(CinemaDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            IEnumerable<Movie> allMovies = this.dbContext.Movies.ToList();
+            return View(allMovies);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Movie movie)
+        {
+            /*if (!ModelState.IsValid)
+            {
+                return View(movie);
+            }*/
+
+            this.dbContext.Movies.Add(movie);
+            this.dbContext.SaveChanges();
+
+            return this.RedirectToAction(nameof(Index));
+        }
+
+        [HttpGet]
+        public IActionResult Details(string id)
+        {
+            bool isGuid = Guid.TryParse(id, out Guid movieId);
+
+            if (!isGuid)
+            {
+                return this.RedirectToAction(nameof(Index));
+            }
+
+            Movie? movie = this.dbContext.Movies.FirstOrDefault(m => m.Id == movieId);
+
+            if (movie == null)
+            {
+                return this.RedirectToAction(nameof(Index));
+            }
+
+            return this.View(movie); 
+        }
+    }
+}
