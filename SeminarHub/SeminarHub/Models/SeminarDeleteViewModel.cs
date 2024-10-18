5e3093940fa36da1700c2699fa5379f0c6b8b3cf
Yavor Petrov <147103195+PetrovKunev@users.noreply.github.com>
2024-10-18 19:22:04 +0300

    Seminar_Final

---
+++ SeminarHub/SeminarHub/Models/SeminarDeleteViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace SeminarHub.Models
+{
+    public class SeminarDeleteViewModel
+    {
+        public int Id { get; set; }
+        public string Topic { get; set; } = null!;
+
+        public DateTime DateAndTime { get; set; } // Добави това свойство
+    }
+}
